e5b72d683532ea6b5d513d74d2ea6d1215138eb3
Yubo-Cao <cao2006721@gmail.com>
2022-09-04 21:49:19 -0400

    fix: removing compilation files

--- generate.py
+++ generate.py
@@ -1,8 +1,7 @@
 from yaml import load, Loader
 from pathlib import Path
-
-out_dir = Path("~/out").expanduser()
-out_dir.mkdir(exist_ok=True)
+from quizlet_helper import Card, StudySet, Folder, User, log
+from playwright.sync_api import sync_playwright
 
 
 def format_type(type):
@@ -23,23 +22,43 @@ def format_synonyms(synonyms):
     return synonyms
 
 
-def format_card(vocab):
-    return f"""{vocab["vocab"]}
-<SEP>
-{format_type(vocab['type'])} {'；'.join(vocab["def"]["cn"])} | {'; '.join(vocab["def"]["en"])}
+def format_card(vocab) -> Card:
+    return Card(
+        vocab["vocab"],
+        f"""{format_type(vocab['type'])} {'；'.join(vocab["def"]["cn"])} | {'; '.join(vocab["def"]["en"])}
 {format_synonyms(vocab.get("synonyms", []))}
-{vocab['example_sentence']}
-<CARD>"""
+{vocab['example_sentence']}""",
+    )
 
 
-with open("refined_formatted.yml") as f:
-    sections = load(f, Loader)
+with (
+    sync_playwright() as playwright,
+    open("vocab.yml", "r") as vocab,
+    open("auth.yml", "r") as auth,
+):
+    auth = load(auth, Loader=Loader)
+    sections = load(vocab, Loader)
+    
+    browser = playwright.chromium.launch(headless=False)
+    user = User(username=auth["username"], password=auth["password"], browser=browser)
+    folder = Folder(user, name="barron")
+    folder.created = True
+
     for section in sections:
-        formatted = []
-        for vocab in section["vocabs"]:
-            try:
+        try:
+            formatted = []
+            for vocab in section["vocabs"]:
                 formatted.append(format_card(vocab))
-            except:
-                print(vocab)
-        with open(out_dir / f"{section['index']}-{section['start']}-{section['end']}.txt", "w") as f:
-            f.write("\n".join(formatted))
+            name = f"{section['index']}: {section['start']}-{section['end']}"
+            set = StudySet(
+                user=user,
+                folders=[folder],
+                name=name,
+                cards=formatted,
+                definition_lang="英语",
+                word_lang="中文（简体）",
+            )
+            set.create()
+            set.page.close()
+        except Exception as e:
+            log.error(f"Error while creating study set for {section['index']}: {e}")
